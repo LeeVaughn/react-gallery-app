3035e6805cb7265d2c834cabab50f10934e9a20f
LeeVaughn <dlvprogramming@gmail.com>
2018-08-31 10:35:42 -0700

    added notes

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// renders the App component to the DOM via the root div
 ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
