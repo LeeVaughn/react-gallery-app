b833c4ef15244741b3479b22bc9606dca387344b
LeeVaughn <dlvprogramming@gmail.com>
2018-08-31 13:45:31 -0700

    changed single parens to double parens

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
 // renders the App component to the DOM via the root div
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 registerServiceWorker();
