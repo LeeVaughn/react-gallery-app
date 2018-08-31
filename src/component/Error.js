29e02b5ead5aa7d581af5c7f933ec9c0c00443d7
LeeVaughn <dlvprogramming@gmail.com>
2018-08-31 11:23:57 -0700

    created initial component

---
+++ src/component/Error.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Error = () => (
+    <div className="photo-container">
+        <h2>Results</h2>
+        <ul>
+            <li className="not-found">
+                <h2>Page Not Found</h2>
+            </li>
+        </ul>
+    </div>
+);
+
+export default Error;
