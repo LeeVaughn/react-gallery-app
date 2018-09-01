691537c825eeba09fbfbe06488d32b9f7125f0cd
LeeVaughn <dlvprogramming@gmail.com>
2018-09-01 15:25:38 -0700

    changed button names

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -36,9 +36,9 @@ export default class Nav extends Component {
 
                 <nav className="main-nav">
                     <ul>
-                        <li><NavLink to="/gallery">Cats</NavLink></li>
+                        <li><NavLink to="/gallery">Pandas</NavLink></li>
                         <li><NavLink to="/gallery">Dogs</NavLink></li>
-                        <li><NavLink to="/gallery">Computers</NavLink></li>
+                        <li><NavLink to="/gallery">Nature</NavLink></li>
                     </ul>
                 </nav>
             </div>
