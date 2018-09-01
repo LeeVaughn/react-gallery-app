719d20d26f39d836a207d5d8901f487b84dadee9
LeeVaughn <dlvprogramming@gmail.com>
2018-09-01 11:22:03 -0700

    added functionality to request data from API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // components
@@ -7,13 +8,45 @@ import Gallery from "./components/Gallery";
 import NotFound from "./components/NotFound";
 import Error from "./components/Error";
 
-class App extends Component {
+import apiKey from "./config.js";
 
-  state = {
-    
+export default class App extends Component {
+
+  // defines initial state
+  constructor() {
+    // allows the use of this inside the construtor within the context of the App class
+    super();
+    this.state = {
+      images: [],
+      loading: true,
+      perPage: 24
+    }
+  }
+
+  // runs as soon as App is mounted to the DOM
+  componentDidMount() {
+    this.performSearch();
+  }
+
+  // handles API request
+  performSearch = (query = "beach") => {
+    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&in_gallery=&per_page=${this.state.perPage}&format=json&nojsoncallback=1`)
+      .then(response => {
+        // handle success
+        this.setState({
+          images: response.data.photos.photo,
+          category: {query},
+          loading: false
+        });
+      })
+      .catch(error => {
+        // handle error
+        console.error("Error fetching and parsing data", error)
+      });
   }
 
   render() {
+    console.log(this.state.images);
     return (
       // renders root router that listens to URL changes
       <BrowserRouter>
@@ -32,5 +65,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
